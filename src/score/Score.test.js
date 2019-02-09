d863029f77146a081116aca1f52320bc836a9a94
Sebastien Bro <sebastien.bro.c@thalesdigital.io>
2019-02-09 14:04:47 +0100

    fix(score): fix ut

--- src/score/Score.test.js
+++ src/score/Score.test.js
@@ -3,15 +3,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Score from './Score';
 
-const score = (
-  <Score
-    players={['Jess', 'Seb']}
-    rounds={[
-      { id: 1, result: [3, 0] },
-      { id: 2, result: [5, 0] },
-    ]}
-  />
-);
+const rounds = [{ id: 1, result: [3, 0] }, { id: 2, result: [5, 0] }];
+
+const score = <Score players={['Jess', 'Seb']} rounds={rounds} />;
 
 it('renders without crashing', () => {
   shallow(score);
@@ -19,5 +13,5 @@ it('renders without crashing', () => {
 
 it('calculates a player score by summing rounds', () => {
   const wrapper = shallow(score);
-  expect(wrapper.instance().calculatePoints(0)).toEqual(8);
+  expect(wrapper.instance().calculatePoints(rounds, 0)).toEqual(8);
 });
