833507ebcb54e91144d63afca976403418ae2004
Sebastien Bro <sebastien.bro.c@thalesdigital.io>
2019-02-09 15:47:32 +0100

    fix(app): fix UT

--- src/App.test.js
+++ src/App.test.js
@@ -1,33 +1,31 @@
 import React from 'react';
 
 import { shallow } from 'enzyme';
-import Game from './Game';
+import App from './App';
 
-const game = (
-  <Game />
-);
+const app = <App />;
 
 it('renders without crashing', () => {
-  shallow(game);
+  shallow(app);
 });
 
 it('should add new rounds', () => {
-  const wrapper = shallow(game);
+  const wrapper = shallow(app);
   wrapper.instance().createRound();
   wrapper.instance().createRound();
   wrapper.instance().createRound();
   expect(wrapper.state().rounds.length).toEqual(3);
 });
 
-it('should set the game type', () => {
-  const wrapper = shallow(game);
+it('should reset the game', () => {
+  const wrapper = shallow(app);
   const gameType = 'Wist';
-  wrapper.instance().setGameType(gameType)();
+  wrapper.instance().resetGame(gameType);
   expect(wrapper.state().gameType).toEqual('Wist');
 });
 
 it('should add a new player', () => {
-  const wrapper = shallow(game);
+  const wrapper = shallow(app);
   // const gameType = 'Wist';
   wrapper.instance().addPlayer('Bro');
   expect(wrapper.state().players[2]).toEqual('Bro');
